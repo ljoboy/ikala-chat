f4205877602755688a5debb5291ff606fec8f513
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-13 23:03:37 +0200

    [NEW] navbar.php

---
+++ _inc/navbar.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-inverse">
+  <ul class="nav navbar-nav">
+    <li><a href="#">Link</a></li>
+    <li><a href="#">Link</a></li>
+  </ul>
+  <p class="navbar-text">Some text</p>
+</nav>
