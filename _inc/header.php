85acdbd2bc788c91a56512226a5265dd89614ba7
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-13 23:03:08 +0200

    [NEW] header.php

---
+++ _inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= $GLOBALS["domain"]."css/bootstrap.css" ?>">
+    <title>Ikala-Chat | <?= isset($title) ? $title : $GLOBALS['tagLine'] ?></title>
+</head>
+<body>
+<?php navbar() ?>
+<div class="container">
