6ab5ac451c328892abff16db909ee09b17350071
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-13 23:04:02 +0200

    [NEW] login.php

---
+++ _statics/login.php
@@ -0,0 +1,26 @@
+<form action="login.html" method="post" class="form-horizontal">
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email ou Pseudo :</label>
+    <div class="col-sm-10">
+      <input type="text" name="email" class="form-control" id="email" placeholder="Entrer votre e-mail ou votre pseudo">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Mot de passe :</label>
+    <div class="col-sm-10"> 
+      <input type="password" class="form-control" id="pwd" placeholder="Mot de passe" name="pwd"/>
+    </div>
+  </div>
+  <!-- <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label><input type="checkbox"> Remember me</label>
+      </div>
+    </div> -->
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Connexion</button>
+    </div>
+  </div>
+</form>
